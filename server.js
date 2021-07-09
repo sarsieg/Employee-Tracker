19bc4f760ca3623b4fe48f62e4c99afea16e81c0
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-08 19:02:33 -0500

    added function to view employees and added a throw error

--- server.js
+++ server.js
@@ -87,3 +87,32 @@ function firstPrompt() {
             }
         });
 }
+
+// view employees
+// READ all, SELECT * FROM 
+function viewEmployee() {
+    console.log("Viewing employees/n");
+
+    var query =
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name AS manager
+    FROM employee e
+    LEFT JOIN role r
+        ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+
+        const departmentChoices = res.map(data => ({
+            value: data.id,
+            name: data.name
+        }));
+
+        console.table(res);
+        console.log("Department view succeed!/n");
+
+        promptDepartment(departmentChoices)
+    });
+}
