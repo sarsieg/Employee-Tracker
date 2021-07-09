84b6bbcca21552e28b0552949fbd782b0d117af7
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-08 19:45:49 -0500

    added inquirer prompt to ask questions to get employee information then take that information and inset a new item into the db

--- server.js
+++ server.js
@@ -197,3 +197,46 @@ function AddEmployee() {
         promptInsert(roleChoices)
     });
 }
+
+function promptInsert(roleChoices) {
+
+    inquirer
+        .promp([{
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?"
+            },
+            {
+                type: "list",
+                name: "roleId",
+                message: "What is the employee's role?",
+                choices: roleChoices
+            },
+        ])
+        .then(function(answer) {
+            console.log(answer);
+
+            var query = `INSERT INTO employee SET ?`
+
+            // when finished prompting insert a new item into the db with the information given
+            connection.query(query, {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    role_id: answer.roleId,
+                    manager_id: answer.managerId,
+                },
+                function(err, res) {
+                    if (err) throw err;
+
+                    console.table(res);
+                    console.log(res.insteredRows + "Inserted successfully!\n");
+
+                    firstPrompt();
+                });
+        });
+}
