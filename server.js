872fc0def6068c388ad204cffe922c9556765d9b
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-08 20:55:26 -0500

    added function to add role

--- server.js
+++ server.js
@@ -382,3 +382,30 @@ function promptEmployeeRole(employeeChoices, roleChoices) {
                 });
         });
 }
+
+// "Add Role" / CREATE: INSERT INTO
+function addRole() {
+
+    var query =
+        `SELECT e.id, d.name, r.salary AS budget
+    FROM employee e
+    JOIN role r
+        ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+
+        const departmentChoices = res.map(({ id, name }) => ({
+            value: id,
+            name: `${id} ${name}`
+        }));
+
+        console.table(res);
+        console.log("Department array!");
+
+        promptAddRole(departmentChoices);
+    });
+}
