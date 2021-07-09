d95554034ce01bc5ad6febdf828853ef275710f1
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-08 20:40:18 -0500

    finished employee array

--- server.js
+++ server.js
@@ -295,3 +295,31 @@ function promptDelete(deletedEmployeeChoices) {
 function updateEmployeeRole() {
     employeeArray();
 }
+
+function employeeArray() {
+    console.log("Updating an employee");
+
+    var query =
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name AS manager
+        FROM employee e
+        JOIN role r
+            ON e.role_id = r.id
+        JOIN department d
+        ON d.id = r.department_id
+        JOIN employee m
+            ON m.id = e.manager_id`
+
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+
+        const employeeChoices = res.map(({ id, first_name, last_name }) => ({
+            value: id,
+            name: `${first_name} ${last_name}`
+        }));
+
+        console.table(res);
+        console.log("employeeArray To Update!\n")
+
+        roleArray(employeeChoices);
+    });
+}
